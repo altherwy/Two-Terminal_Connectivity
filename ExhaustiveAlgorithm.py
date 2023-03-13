e2e95a3584d0da4ecee01dc3a7fb5ed6338afdf9
Youssef Altherwy <ysf200@gmail.com>
2023-03-13 08:50:12 +0300

    Update ExhaustiveAlgorithm.py
    Adding connection path from S to T

--- ExhaustiveAlgorithm.py
+++ ExhaustiveAlgorithm.py
@@ -13,30 +13,37 @@ loc_links = pd.DataFrame({('S','A'): {0:[1,0], 1:[0,1]},
                           ('B','T'): {0:[1,1], 1:[0,1]},
                           })
 prob= pd.DataFrame({}, columns=['S','A','B','T','Prob'])
+conn_prob = pd.DataFrame({}, columns=['S','A','B','T','Prob'])
 
-def rec(node_ind:int, s_p:int, loc_ind:int, path:Dict):
+def rec(node_ind:int, s_p:int, loc_ind:int, path:Dict, conn_path:Dict):
     node:str = nodes[node_ind]
     prev_node:str = nodes[node_ind-1]
-    path[prev_node] = loc_ind
-
+    #path[prev_node] = loc_ind
+    
     node_loc:List = loc[node]
 
 
     for j in range(len(node_loc)):
+        conn_path[node] = j
         p = node_loc[j]
         s_p *= p
         path:Dict = __check_connection(node,j,path)
-        print(path)
         if node != 'T':
             node_ind += 1 # next node in nodes list
-            node_ind= rec(node_ind,s_p,j,path)
+            node_ind= rec(node_ind,s_p,j,path, conn_path)
             s_p /= p
         else:
             
             indices = [v for k,v in path.items()]
             indices.append(s_p)
             prob.loc[len(prob.index)] = indices
+  
+            indices = [v for k,v in conn_path.items()]
+            indices.append(s_p)
+            conn_prob.loc[len(conn_prob.index)] = indices
+            
             path.popitem() # remove last node
+            conn_path.popitem()
             s_p /= p # remove the probability of the last node
 
     
@@ -64,9 +71,10 @@ def __check_connection(node:str, index:int, path:Dict)->Dict:
 node_loc:List = loc['S']
 for i in range(len(node_loc)):
     path:Dict = {'S':i}
-    rec(1,node_loc[i],i,path)
+    conn_path:Dict = {'S':i}
+    rec(1,node_loc[i],i,path, conn_path)
 # %%
-print(prob)
+print(conn_prob)
 # %%
 indices = prob.loc[0]
 print(indices['S'])
