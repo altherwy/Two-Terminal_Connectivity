26b100d61033f7253fab838f9249a2f97905e5cb
Youssef Altherwy <ysf200@gmail.com>
2023-06-11 18:33:39 +0300

    change node_ids to chars

--- physical_model.py
+++ physical_model.py
@@ -33,28 +33,53 @@ to_list = []
 from_loc_list = []
 to_loc_list = []
 status_list = []
+prob_list = []
+
 for key in loc_links.keys():
     key_list = list(key)
     node_from = key[0]
     node_to = key[1]
     dict_ = loc_links[key]
-    for loc in dict_.keys():
-        list_ = dict_[loc]
+    for locality in dict_.keys():
+        list_ = dict_[locality]
         counter = 0
         for item in list_:
             from_list.append(node_from)
             to_list.append(node_to)
-            from_loc_list.append(loc)
+            from_loc_list.append(locality)
             to_loc_list.append(counter)
             status_list.append(item)
+            if item == 1: # if link is active
+                prob = loc[node_from][locality]*loc[node_to][counter]
+            else:
+                prob = 0 # if link is inactive
+            prob_list.append(prob)
             counter += 1
 
-df_loc_links = pd.DataFrame({'from':from_list,'to':to_list,'from_loc':from_loc_list,'to_loc':to_loc_list,'status':status_list})           
-
-
+df_loc_links = pd.DataFrame({'from':from_list,'to':to_list,'from_loc':from_loc_list,'to_loc':to_loc_list,'prob':prob_list,'status':status_list})           
 
 # %%
-sim.node_positions_filtered
+df_ = pd.DataFrame()
+df_['from'] = df_loc_links['from'].astype(int)
+# change to string starting from A, B, ...
+df_['from'] = df_['from'].apply(lambda x: chr(x+65))
+df_['to'] = df_loc_links['from_loc']
+# add from char before number. For example A0, A1, ...
+df_['to'] = df_['from'] + df_['to'].astype(str)
+df_ = df_.drop_duplicates()
+# restart the index from 0
+df_ = df_.reset_index(drop=True)
+df_
 # %%
-df_loc_links[df_loc_links['status']==1]
+df_node = pd.DataFrame({'id':[i for i in range(sim.number_of_nodes-1)]})
+df_node['id'] = df_node['id'].apply(lambda x: chr(x+65))
+Jaal(df_).plot()
+# %%
+links
+
+# %%
+loc
+# %%
+loc_links
+
 # %%
