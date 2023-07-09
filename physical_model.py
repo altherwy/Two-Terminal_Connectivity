3c5a4dc6d6159a726e65e7357c22ab93ca97f502
Youssef Altherwy <ysf200@gmail.com>
2023-07-09 17:04:43 +0300

    finish two terminal

--- physical_model.py
+++ physical_model.py
@@ -1,6 +1,6 @@
 #%%
 import physical_model_simulation as pms
-number_of_nodes = 6
+number_of_nodes = 8
 number_of_localities = 3
 phys_model = pms.PhysicalModel(number_of_nodes = number_of_nodes, loc_set_max=number_of_localities)
 loc, links,loc_links, nodes = phys_model.get_data()
@@ -14,5 +14,22 @@ ea = ex_algthm.ExhaustiveAlgorithm(nodes = nodes, loc = loc, links = links, loc_
 ea.main()
 
 # %%
-ea.paths[ea.paths['Connected'] == False]
+ea.paths[ea.paths['Connected'] == True]
+# %%
+import TwoTerminalConn as ttc
+paths = ea.paths.copy()
+two_ter_conn = ttc.TwoTerminal(links=links, loc=loc, loc_links=loc_links,paths= paths)
+conn = two_ter_conn.main()
+print('Two Terminal Conn: ', conn)
+# %%
+two_ter_conn.dps
+# %%
+phys_model.node_positions_filtered.head()
+# %%
+# sum pos_prob based on node_id
+phys_model.node_positions_filtered.groupby('node_id')['pos_prob'].sum()
+
+
+# %%
+loc_links
 # %%
